c882fd69653c3b50aec2af779890056f4b0ddb25
johncscott <john@c3x.co.uk>
2015-02-12 15:09:00 +0000

    Simple DLL as discussed here https://our.umbraco.org/forum/umbraco-pro/contour/61132-cookie-contour_xxxxxxxxx

---
+++ src/SecureCookies/Global.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+
+namespace SecureCookies
+{
+    public class Global : Umbraco.Web.UmbracoApplication
+    {
+      public void Init(HttpApplication application)
+      {
+          application.EndRequest += (new EventHandler(this.Application_EndRequest));
+      }
+    
+      private void Application_EndRequest(object sender, EventArgs e)
+      {
+          if (Response.Cookies.Count > 0)
+            {
+                foreach (string s in Response.Cookies.AllKeys)
+                {
+                         Response.Cookies[s].Secure = true;
+                         Response.Cookies[s].HttpOnly = true;
+                }
+            }
+      }
+
+    }
+}
+
